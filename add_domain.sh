89f2cd64f28f1f70b37a78447af5714328c3ebba
Harry MOTHERFUCKIN Potter, G <Harry@HarryEffingPotter.com>
2025-02-24 00:42:57 -0500

    Fixed

--- add_domain.sh
+++ add_domain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Not running as root"
     exit 1
@@ -14,12 +15,15 @@ PORT=$2
 
 
 echo "Registering the domain with certbot..."
-sudo certbot certonly -d $1 --non-interactive --agree-tos -m thisisyourknife@gmail.com --standalone 
-if [ "$?" -neq 0 ]; then
+if ! sudo certbot certonly --non-interactive --nginx -d "${1}" --agree-tos -m "${email}" --force-renewal
+then
     echo -e "Certbot failed, attempt runn ing the following line manually and viewing the output:\n\nsudo certbot -d $1\n\n"
     sleep 3
-    echo "Now exiting..."
-    exit 1
+    echo "Do you still want to create config file in /etc/nginx/conf.d (if you plan on manually certifying as suggested) [y/n]?"
+    read response
+    if [[ ${response} == "N" ]] || [[ ${response} == "n" ]]; then
+        exit
+    fi
 fi
 # Read the template and replace placeholders with actual values
 CONFIG=$(cat nginx_template.conf | sed "s/__DOMAIN__/$DOMAIN/g" | sed "s/__PORT__/$PORT/g")
@@ -27,6 +31,7 @@ CONFIG=$(cat nginx_template.conf | sed "s/__DOMAIN__/$DOMAIN/g" | sed "s/__PORT_
 # Save thegenerated config to a new file
 CONFIG_FILE="/etc/nginx/conf.d/$DOMAIN.conf"
 echo "$CONFIG" > $CONFIG_FILE
+echo "Config file created at ${CONFIG_FILE}"
 
 echo "Added $1 to config! Restarting service now!"
 
--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
-#!/usr/bin
+#!/bin/bash
 current_directory="$(pwd)"
-echo "Backing up nginx config..."
+echo "Backing up your current nginx config to /etc/nginx/nginx.conf.backup, if there are lines you need from it you will have to implement it yourself!"
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
+if ! ls /etc/nginx/nginx.conf > /dev/null; then
+    echo "nginx config missing, cant back it up!"
+fi
+if ! ls /etc/nginx > /dev/null; then
+    echo "nginx folder missing, please install nginx"
+    exit 0
+fi
+
 echo "Moving nginx.conf template to /etc/nginx"
 sudo cp -a "${current_directory}"/nginx.conf /etc/nginx/
 echo "Moving add_domain to /usr/bin"
 sudo cp -a  "${current_directory}"/add_domain.sh /usr/bin/add-domain
+while true; do
+    echo "What email do you want to use for registering these subdomains?"
+    read email
+    if [ -n $email ]; then
+        echo "email=$email" > "$(pwd)/.env"
+        break
+    fi
+    echo "You didn't enter anything?"
+done
 echo "Finished setup"
 echo -e "\nUsage:\nsudo add-domain subdomain.domain.com <port number of service>\nsudo add-domain test.mydomain.net 6969"
+
